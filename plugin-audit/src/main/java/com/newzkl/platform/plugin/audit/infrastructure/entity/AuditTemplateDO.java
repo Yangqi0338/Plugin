c8a3e0929b8ddfd6d860708df62a5041877d6acb
KC <kchange0915@gmail.com>
2026-08-10 10:36:13 +0800

    v1.0.0

--- plugin-audit/src/main/java/com/newzkl/platform/plugin/audit/infrastructure/entity/AuditTemplateDO.java
+++ plugin-audit/src/main/java/com/newzkl/platform/plugin/audit/infrastructure/entity/AuditTemplateDO.java
@@ -1,9 +1,11 @@
 package com.newzkl.platform.plugin.audit.infrastructure.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.newzkl.platform.base.common.core.mybatis.handler.RawJsonStringTypeHandler;
 import com.newzkl.platform.base.common.ddd.infrastructure.mybatis.model.AuditBaseDO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.dromara.mpe.autofill.annotation.JsonSerializable;
 
 /**
  * 审批模板
@@ -12,7 +14,7 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-@TableName
+@TableName(autoResultMap = true)
 public class AuditTemplateDO extends AuditBaseDO {
 
     /** 模板类型 1 角色申请 2 保证金 3 SPU上传 4 品牌申请 5 SPU工单 6 实名认证 */
@@ -22,8 +24,10 @@ public class AuditTemplateDO extends AuditBaseDO {
     private String name;
 
     /** 审批节点信息 JSON */
+    @JsonSerializable(typeHandler = RawJsonStringTypeHandler.class)
     private String node;
 
     /** 审批节点流转信息 JSON */
+    @JsonSerializable(typeHandler = RawJsonStringTypeHandler.class)
     private String edge;
 }
